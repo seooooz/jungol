26bd6567ec5bf78faf14ab9754a5bad8357b551f
ju <107034875+seooooz@users.noreply.github.com>
2022-06-10 15:06:08 +0900

    2nd commit

--- Main.java
+++ Main.java
@@ -4,22 +4,22 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int[] arr = new int[10];
-		int sum = 0;
-		int sum1 = 0;
+		int even = 0;
+		int odd = 0;
 		double avg = 0;
 
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
 
-			if (i % 2 == 0) { // È¦¼ö
-				sum1 += arr[i];
-				avg = (double) sum1 / 5;
+			if (i % 2 == 0) { // odd
+				odd += arr[i];
+				avg = (double) odd / 5;
 			} else
-				sum += arr[i];
+				even += arr[i];
 		}
 		sc.close();
 
-		System.out.println("sum : " + sum);
+		System.out.println("sum : " + even);
 		System.out.println("avg : " + avg);
 
 	}
